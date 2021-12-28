002054a9c43c00e129171ee0a4a0a3fff3833b48
nicolas <nicoranalli9@gmail.com>
2021-12-28 11:05:32 -0300

    commit desde el laburo 2

---
+++ src/main/java/clases/Aritmetica.java
@@ -0,0 +1,13 @@
+package clases;
+
+
+
+public class Aritmetica {
+    int a;
+    int b;
+    
+    public void sumar(){
+        int resultado = a + b;
+        System.out.println(resultado);
+    }
+}
---
+++ src/main/java/clases/Prueba.java
@@ -0,0 +1,12 @@
+package clases;
+
+public class Prueba {
+    public static void main(String[] args) {
+        Aritmetica arit1 = new Aritmetica();
+        arit1.a = 2;
+        arit1.b = 4;
+
+        arit1.sumar();
+
+    }
+}
